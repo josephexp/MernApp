476e06c969d391406f51ecacbfb35e4d8b1f78b4
josephexp <joseph.antony@experionglobal.com>
2021-05-16 15:06:53 +0530

    fixes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const mongoose = require('mongoose');
 var cors = require('cors');
 var app = express();
 const dotenv = require('dotenv');
@@ -10,7 +11,6 @@ const routes = require('./routes');
 
 dotenv.config();
 app.use(cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // convert error to ApplicationError, if needed
@@ -18,23 +18,18 @@ app.use(errorConverter);
 app.use(errorHandler);
 
 app.use('/', routes);
-// app.get('/', function (req, res) {
-// 	res.send('Hello World');
-// });
 
-var server = app.listen(8081, function () {
-	var host = server.address().address;
-	var port = server.address().port;
-
-	console.log('Example app listening at http://%s:%s', host, port);
-});
-
-//Import the mongoose module
-var mongoose = require('mongoose');
-
-//Set up default mongoose connection
+let server;
 var mongoDB = process.env.MONGOURI;
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+let port = 8081;
+mongoose
+	.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		console.log(`Connected to mongo`);
+		server = app.listen(port, () => {
+			console.log(`Server running on ${port}`);
+		});
+	});
 
 //Get the default connection
 var db = mongoose.connection;
@@ -42,93 +37,34 @@ var db = mongoose.connection;
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-/*
-var myobj = { email: "Company Inc", password: "Highway 37" };
-db.collection("mern-app").insertOne(myobj, function (err, res) {
-  if (err) throw err;
-  console.log("1 document inserted");
-  db.close();
-});
-
-
-db.collection("mern-app").findOne(
-  { email: "Company Inc" },
-  function (err, res) {
-    if (err) throw err;
-    console.log(res);
-    db.close();
-  }
-);
-
-*/
-
-// app.get("/api", async (req, res) => {
-//   const surveys = await db
-//     .collection("mern-app")
-//     .findOne({ email: "Company Inc" });
-
-//   res.send(surveys);
-// });
-
-// app.post("/api", async (req, res) => {
-//   console.log(req.body);
-//   const { email, password } = req.body;
-//   const surveys = await db
-//     .collection("mern-app")
-//     .findOne({ email: email, password: password }, function (err, res2) {
-//       if (err) {
-//         res2.status(400).send({
-//           success: false,
-//           message: "login failed",
-//         });
-//       }
-//     });
-//   res.send(surveys);
-// });
-
-app.post('/api', async (req, res) => {
-	console.log(req.body);
-	const { email, password } = req.body;
-	const surveys = await db
-		.collection('mern-app')
-		.findOne({ email: email, password: password });
-	if (surveys) {
-		res.send(surveys);
+const exitHandler = () => {
+	if (server) {
+		server.close(() => {
+			console.log('Server closed');
+			process.exit(1);
+		});
 	} else {
-		res.status(401).send({
-			success: false,
-			message: 'login failed',
-		});
+		process.exit(1);
 	}
-});
+};
 
-app.post('/register', async (req, res) => {
-	console.log(req.body);
-	const { email, password } = req.body;
-	db.collection('mern-app')
-		.insertOne({ email, password })
-		.then(() => {
-			console.log('1 document inserted');
-			db.close();
-			res.status(200).send({
-				success: true,
-				message: 'Registration Successful',
-				data: { email: email, password: password },
-			});
-		})
-		.catch((error) => {
-			res.status(200).send({
-				success: false,
-				message: 'Registration unSuccessful',
-			});
-		});
-});
+const unexpectedErrorHandler = (error) => {
+	console.log(error);
+	exitHandler();
+};
 
-// var myobj = { email: "Company Inc", password: "Highway 37" };
-// db.collection("mern-app").insertOne(myobj, function (err, res) {
-//   if (err) throw err;
-//   console.log("1 document inserted");
-//   db.close();
-// });
+// Function to gracefully close mongoose connection
+const gracefullyCloseConnection = async () => {
+	mongoose.connection.close(() => {
+		console.log('MongoDB connection is closed');
+		process.exit(0);
+	});
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', gracefullyCloseConnection);
+process.on('SIGINT', gracefullyCloseConnection);
 
 module.exports = app;
